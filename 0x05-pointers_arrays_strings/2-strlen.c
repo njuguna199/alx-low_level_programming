67cd5e7b39aaf574466b440b3b93122708e89f50
njuguna199 <kimaninjuguna199@gmail.com>
2022-06-14 15:23:06 -0700

    pointers arrays

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include <string.h>
+
+/**
+ * _strlen - prints lengths of string
+ * @s: string to be evaluated
+ *
+ * Return: returns size of string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * _puts - writes to stdout
+ * @str: string to be printed
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
