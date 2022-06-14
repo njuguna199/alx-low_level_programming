734b1bbcf5346c242edcaa2617ef0055aa88a54e
njuguna199 <kimaninjuguna199@gmail.com>
2022-06-14 15:12:29 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,6 @@
-#include "main.h"
-
 /**
- * reset_to_98 - resets value of pointer to 98
- * @n: pointer to reset to 98
- *
- * Return: void
+ * reset_to_98 - change to 98
+ * @n: pointer to be changed
  */
 void reset_to_98(int *n)
 {
